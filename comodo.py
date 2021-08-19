ac32a2cf01d9b01ebaa660cc5796a119f3f9f7c4
EmillyLopes <em.trabalho@outlook.com>
2021-08-19 13:02:35 -0300

    primeira versão

---
+++ comodo.py
@@ -0,0 +1,33 @@
+class Comodo:
+    __largura:float
+    __profundidade: float
+    __altura: float
+
+    def __init__(self, largura, profundidade):
+        self.largura = largura
+        self.profundidade = profundidade
+        self.__altura= 2.9
+
+    @property
+    def largura(self):
+        return self.__largura
+
+    @property
+    def profundidade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @largura.setter
+    def largura(self, largura):
+        self.__largura = float(largura)
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        try:
+            self.__profundidade = float(profundidade)
+        except Exception:
+            print("O valor informado da pronfudidade é invalido")
+
